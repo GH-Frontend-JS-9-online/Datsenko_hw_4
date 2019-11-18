efbf8cb06831fa3e29bc0d8e9a24ed2da220eea4
Yevhen Datsenko <gk16121983@gmail.com>
2019-11-18 18:01:14 +0200

    refactor hw4

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,40 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
-    entry: './src/js/index.js',
+    entry: './src/index.js',
+    devServer: {
+        contentBase: './dist',
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'App Forkio',
+            template: './src/index.html',
+            filename: 'index.html',
+        }),
+    ],
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
     },
     module: {
         rules: [{
-            test: /\.css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-            ],
-        }, ],
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                ],
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                loader: 'file-loader?name=/img/[name].[ext]'
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                loader: 'file-loader?name=/fonts/[name].[ext]'                
+            },
+        ],
     },
 }
